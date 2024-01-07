8f7558974361095237423ee5a171b7b3e2238f81
Thiago Rodrigues Teixeira <thiagotx2019@gmail.com>
2024-01-07 09:33:05 -0300

    Atualizando a página

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-$(document).ready(function () {
-    $("a").on("click", function (event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-      $("html, body").animate(
-          {
-            scrollTop: $(hash).offset().top,
-          },
-          800,
-          function () {
-            window.location.hash = hash;
-          }
-        );
-      }
+//Função que redireciona para o contato de whatsapp.
+document.addEventListener("DOMContentLoaded", function() {
+
+  const whatsapp = document.getElementById('whatsappIcon');
+
+  if (whatsapp) {
+
+    whatsapp.addEventListener('click', function() {
+
+      window.open('https://wa.me/5564996753372');
+
     });
-  });
+
+  } else {
+
+    console.log("Elemento whatsappIcon não encontrado.");
+
+  }
+
+});
